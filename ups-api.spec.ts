24c932ea48789084cc842880de6b0ef1097f9abc
Alessandro Rubino <aleruby97@hotmail.it>
2023-03-28 17:56:56 +0200

    Test automatic authentication

--- ups-api.spec.ts
+++ ups-api.spec.ts
@@ -83,6 +83,89 @@ describe("UPSApi", () => {
         });
     });
 
+    describe("Automatic authentication", () => {
+        let mockGenerateToken: jest.Mock;
+        let mockRate: jest.Mock;
+        let mockShipment: jest.Mock;
+
+        beforeEach(() => {
+            mockGenerateToken = jest.fn();
+            mockRate = jest.fn();
+            mockShipment = jest.fn();
+            jest.doMock("./authentication", () => ({
+                DefaultApi: jest.fn(() => ({
+                    generateToken: mockGenerateToken,
+                })),
+            }));
+            jest.doMock("./rating", () => ({
+                DefaultApi: jest.fn(() => ({
+                    rate: mockRate,
+                })),
+            }));
+            jest.doMock("./shipping", () => ({
+                DefaultApi: jest.fn(() => ({
+                    shipment: mockShipment,
+                    voidShipment: jest.fn(),
+                    labelRecovery: jest.fn(),
+                })),
+            }));
+        });
+
+        it("Sub-api methods are wrapped in authentication checks", async () => {
+            const apiConstructor = require("./ups-api").UPSApi as typeof UPSApi;
+
+            const url = "https://hello.com";
+            const credentials = { client_id: "id", client_secret: "secret" };
+            const api = new apiConstructor(credentials, url);
+            const rating = api.rating();
+            await rating.rate(
+                {
+                    RateRequest: {
+                        Request: { RequestOption: "Rate" },
+                        Shipment: {
+                            Shipper: {
+                                Address: {
+                                    CountryCode: "IT",
+                                    AddressLine: "Via via",
+                                },
+                            },
+                            ShipTo: {
+                                Address: {
+                                    CountryCode: "IT",
+                                    AddressLine: "Via buona",
+                                },
+                            },
+                            _Package: [{}],
+                        },
+                    },
+                },
+                "v1",
+                "Rate"
+            );
+            expect(mockGenerateToken).toHaveBeenCalledTimes(1);
+        });
+
+        it("Multiple calls to sub-apis reuse the same token", async () => {
+            mockGenerateToken.mockImplementation(async () => ({
+                accessToken: "abcd",
+                issuedAt: new Date().toISOString(),
+                expiresIn: "60",
+            }));
+            const apiConstructor = require("./ups-api").UPSApi as typeof UPSApi;
+
+            const url = "https://hello.com";
+            const credentials = { client_id: "id", client_secret: "secret" };
+            const api = new apiConstructor(credentials, url);
+            const rating = api.rating();
+            const shipping = api.shipping();
+            //@ts-ignore
+            await rating.rate();
+            //@ts-ignore
+            await shipping.shipment();
+            expect(mockGenerateToken).toHaveBeenCalledTimes(1);
+        });
+    });
+
     it("Caches sub-apis to only create one instance", () => {
         const rating1 = api.rating();
         const rating2 = api.rating();
